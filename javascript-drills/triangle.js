ee5f776ac6ac71dedff2bd240760dc1decc51e74
oanhcodes <linda.oanh.ho@gmail.com>
2016-06-16 14:39:50 -0500

    Create a valid triangle function

--- javascript-drills/triangle.js
+++ javascript-drills/triangle.js
@@ -1,3 +1,10 @@
 function validTriangle(a, b, c) {
-  // Your code goes here!
+  if (a === b && b === c) {
+    return true;
+  } else if (a === 0 || b === 0 || c === 0) {
+    return false;
+  } else if (Math.pow(a, 2) + Math.pow(b, 2) === Math.pow(c, 2)); {
+    return true;
+  }
 }
+
