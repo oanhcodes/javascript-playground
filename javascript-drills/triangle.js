6611875478cd1445ff593faffaf1bb0cbf6caf3b
oanhcodes <linda.oanh.ho@gmail.com>
2016-06-16 15:02:17 -0500

    Add comments to clarify properties of a valid triangle

--- javascript-drills/triangle.js
+++ javascript-drills/triangle.js
@@ -1,10 +1,13 @@
+// Triangle is valid if no sides have a length equal to 0
+// Triangle is valid if all sides are equal length
+// Triangle is valid is square of a + square of b = sqauare of c
+
 function validTriangle(a, b, c) {
-  if (a === b && b === c) {
-    return true;
-  } else if (a === 0 || b === 0 || c === 0) {
+  if (a === 0 || b === 0 || c === 0) {
     return false;
+  } else if (a === b && b === c) {
+    return true;
   } else if (Math.pow(a, 2) + Math.pow(b, 2) === Math.pow(c, 2)); {
     return true;
   }
 }
-
